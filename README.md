b1a0b0f4b22d9bff573769183175d359fe4eaa42
Fatemeh Abtahi <ftmh.a78@gmail.com>
2024-02-24 21:19:09 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# template
+installed webpack, webpack-cli, html-webpack-plugin, css-loader, style-loader, airbnb eslint, eslint-config-prettier.
+
+initialized webpack with css & img bundle, dev & prod config files, html template.
+
+added package.json scripts (start, build, debug, pages)
+
+should use with npm install
---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+     {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
+      }
+    },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'head',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
