bca3ed01c5663cf615155bb3e0f5c1588f013c9d
ftmhabt <ftmh.a78@gmail.com>
2024-02-25 23:49:48 +0330

    Add illustrations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './style.css';
+import rainy from './rainy.png';
+import cloudy from './cloudy.png';
+import sunny from './sunny.png';
+import snowy from './snowy.png';
+import night from './night.png';
+import half from './half.png';
+import thunder from './thunder.png';
 
 async function getGeo(city) {
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=ed5ff69a9a55d7bb14fda92a70690f61`,
@@ -19,63 +26,77 @@ async function getWeather(city, unit) {
 }
 
 
-let city='london';
-let unit='metric';
+let city = 'london';
+let unit = 'metric';
 
 
 const tempBtn = document.querySelector('.temp');
 
 const cityBtn = document.querySelector('.city');
 
-const searchBox=document.querySelector('.search');
-const exitBtn=document.querySelector('.exit');
-const input=document.querySelector('input');
-const selectBtn=document.querySelector('.select');
+const searchBox = document.querySelector('.search');
+const exitBtn = document.querySelector('.exit');
+const input = document.querySelector('input');
+const selectBtn = document.querySelector('.select');
 
+const img = document.querySelector('img');
 
 
 async function render() {
 
-    
+
     const weatherJson = await getWeather(city, unit);
 
     console.log(weatherJson);
     const temp = weatherJson.main.feels_like;
-    // const weather = weatherJson.weather[0].main;
+    const weather = weatherJson.weather[0].main;
 
+
+    console.log(weather);
+
+    switch (weather) {
+        case 'Cloud':
+            img.src = cloudy;
+            break;
+        case 'Rain':
+            img.src = rainy;
+            break;
+        case 'Snow':
+            img.src = snowy;
+            break;
+        default:
+            img.src = sunny;
+    }
     cityBtn.textContent = city;
+    tempBtn.textContent = `${temp} ${unit === 'metric' ? 'c' : 'f'}`;
 
 
-
-    tempBtn.textContent =`${temp} ${unit==='metric'?'c':'f'}`;
-
-    
 }
 
 
 render();
 
-cityBtn.addEventListener('click',()=>{
+cityBtn.addEventListener('click', () => {
     searchBox.classList.remove('hide');
 })
 
-exitBtn.addEventListener('click',()=>{
+exitBtn.addEventListener('click', () => {
     searchBox.classList.add('hide');
 })
 
-selectBtn.addEventListener('click',()=>{
-    city=input.value;
+selectBtn.addEventListener('click', () => {
+    city = input.value;
     render();
-    input.textContent='';
+    input.textContent = '';
     searchBox.classList.add('hide');
 })
 
 tempBtn.addEventListener('click', () => {
     if (unit === 'metric') {
-        unit='imperial';
+        unit = 'imperial';
     }
     else {
-        unit= 'metric';
+        unit = 'metric';
     }
     render();
 })
